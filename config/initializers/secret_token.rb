1eb2913c899ef1f576ef2d1cf345aafc34d05217
Shawna C Scott <shawnatyger@gmail.com>
2013-12-04 15:42:40 -0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Capstone::Application.config.secret_key_base = '9ad807ec2d0398a3cf407e7410d83b750d9743ea7dc9586d70e6973382763d1359416fb842213376e51ccae1e53d51103fd50c31a284ecfeb687afbeddae3372'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Capstone::Application.config.session_store :cookie_store, key: '_capstone_session'
