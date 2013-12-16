885e4ed2d355f8bb38a802507e0952cb4a639dbb
Shawna C Scott <shawnatyger@gmail.com>
2013-12-16 14:45:37 -0800

    Add spec_helper.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  # Checks for pending migrations before tests are run.
+  ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+  RSpec.configure do |config|
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    config.use_transactional_fixtures = true
+    config.infer_base_class_for_anonymous_controllers = false
+    config.order = "random"
+    config.include Capybara::DSL
+  end
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
+end
