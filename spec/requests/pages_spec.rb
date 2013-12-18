50aa2971529296bc13c39202ab0d2bd7a26b7c52
Shawna C Scott <shawnatyger@gmail.com>
2013-12-17 16:15:11 -0800

    Remove unnecessary tests.

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -9,17 +9,4 @@ describe "Pages" do
     end
   end
 
-  describe "Sign up page" do
-    it "exists" do
-      get signup_path
-      response.status.should be(200)
-    end
-  end
-
-  describe "Sign in page" do
-    it "exists" do
-      get signin_path
-      response.status.should be(200)
-    end
-  end
 end
