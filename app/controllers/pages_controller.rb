4a579c7274b0bbb3535940eafecfd3edd81736df
Shawna C Scott <shawnatyger@gmail.com>
2013-12-17 16:10:12 -0800

    Delete unnecessary views, routes, and controllers after adding devise. Fix "Sign out" link. Make header links appear and disappear appropriately based on sign in.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,4 @@
 class PagesController < ApplicationController
-
-
   def home
   end
-
-  def signin
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,4 @@ Capstone::Application.routes.draw do
   devise_for :users
 
   root 'pages#home'
-
-  match '/users/signup', to: 'devise/sessions#new',     via: 'get'
-  match '/users/signin', to: 'devise/sessions#create',  via: 'post'
-  
 end
